608d0d1e6251253bb1f8c63b19f913cf5e288fbf
lcy <8410294@naver.com>
2023-11-14 20:40:19 +0900

    feat: layout

--- themes/advanced/src/config/defaultConfig.ts
+++ themes/advanced/src/config/defaultConfig.ts
@@ -3,13 +3,13 @@ import { SiteConfig } from "./types";
 const config: SiteConfig = {
   // Website configuration
   website: {
-    title: "Gatsby Advanced Starter", // Homepage title
-    titleShort: "Advanced Blog", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation
-    name: "Gatsby Advanced Starter", // Website name used for homescreen (PWA) and SEO
-    description: "A GatsbyJS starter equipped with advanced features.", // Website description used for RSS feeds/meta description tag
-    language: "en", // Sets the global HTML lang attribute
+    title: "LEECY's Blog", // Homepage title
+    titleShort: "LEECY's Blog", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation
+    name: "LEECY's Blog", // Website name used for homescreen (PWA) and SEO
+    description: "This is LEECY's Blog", // Website description used for RSS feeds/meta description tag
+    language: "kr", // Sets the global HTML lang attribute
     logoUrl: "/logos/logo-1024.png", // Logo used for SEO
-    url: "https://example.com", // Domain of your website without the pathPrefix
+    url: "https://lcy042000.github.io", // Domain of your website without the pathPrefix
     rss: "/rss.xml", // Path to the RSS file
     rssTitle: "Gatsby Advanced Starter RSS Feed", // Title of the RSS feed
     copyright: "© Copyright 2021", // Copyright string for the footer of the website and RSS feed.
