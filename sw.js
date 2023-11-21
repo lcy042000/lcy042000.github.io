f8d1250439c14b86996464ca3c2e709038ac0e62
lcy <8410294@naver.com>
2023-11-21 21:21:45 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ca31663ac2f7186cdfc1.js"
+    "url": "webpack-runtime-4c7f8ddf63c1b3e158cc.js"
   },
   {
     "url": "framework-6780028e2e03c4533616.js"
@@ -39,15 +39,15 @@ self.__precacheManifest = [
     "url": "532a2f07-0ed46d6c1056d01c5554.js"
   },
   {
-    "url": "app-50c137f627e88fc0af33.js"
+    "url": "app-385bad1d7ca6607b2a8b.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "054b95f33a3f70ca790be0afb3c45353"
+    "revision": "f38d74b13e471aecd0883a50c5b0888f"
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "07c3034b0c3225baf42c3903cd8abb63"
+    "revision": "846847eeb794af847fd9cb546c5965ea"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
@@ -157,7 +157,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-50c137f627e88fc0af33.js`))) {
+  if (!resources || !(await caches.match(`/app-385bad1d7ca6607b2a8b.js`))) {
     return await fetch(event.request)
   }
 
