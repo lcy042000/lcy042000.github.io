14793e24fb1ebfd815a39bf0c5a778ce1fe30844
lcy <8410294@naver.com>
2023-11-25 00:17:58 +0900

    fix: config setting

--- themes/advanced/src/config/defaultConfig.ts
+++ themes/advanced/src/config/defaultConfig.ts
@@ -7,11 +7,14 @@ const config: SiteConfig = {
     titleShort: "YOU__P log", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation
     name: "LEECY's log", // Website name used for homescreen (PWA) and SEO
     description: "This is LEECY's Blog", // Website description used for RSS feeds/meta description tag
-    language: "kr", // Sets the global HTML lang attribute
+    language: "ko", // Sets the global HTML lang attribute
     logoUrl: "/logos/leecy-logo-512W.png", // Logo used for SEO
+    fbAppId: "",
+    twitterName: "",
     url: "https://lcy042000.github.io", // Domain of your website without the pathPrefix
     rss: "/rss.xml", // Path to the RSS file
     rssTitle: "Gatsby Advanced Starter RSS Feed", // Title of the RSS feedW
+    googleAnalyticsId: "",
     copyright: "© Copyright 2021", // Copyright string for the footer of the website and RSS feed.
     themeColor: "#D83850", // Used for setting manifest and progress theme colors.
     backgroundColor: "#F7F7F7", // Used for setting manifest background color.
@@ -19,15 +22,16 @@ const config: SiteConfig = {
 
   // User configuration
   user: {
-    about: "안녕하세요. front-end 개발자 이충엽입니다.",
-    avatar: "../advanced/static/avatar/avatar.jpg",
+    id: "lcy042000", // Unique identifier of the user on the website
     firstName: "ChoongYoup",
+    lastName: "Lee",
+    twitterName: "",
+    linkedIn: "www.linkedin.com/in/충엽-이-28a154283",
     github: "lcy042000",
     email: "8410294@naver.com",
-    id: "lcy042000",
-    lastName: "Lee",
-    linkedIn: "www.linkedin.com/in/충엽-이-28a154283",
     location: "koera",
+    about: "안녕하세요. front-end 개발자 이충엽입니다.",
+    avatar: "../advanced/static/avatar/avatar.jpg",
   },
 
   // Organization information used for SEO
